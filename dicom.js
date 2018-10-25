623118a3ce84501ebfaaabd14c7c131af9abbadd
Grimmer <grimmer0125@gmail.com>
2018-10-25 18:47:08 +0800

    Move fetch part to dicom.js and remove using storage part

--- dicom.js
+++ dicom.js
@@ -1,3 +1,22 @@
+function fetchLocalFile(url) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function(e) {
+    // TODO: change to the real dicom image content 
+    console.log("load xhr text :", xhr.responseText);
+    console.log("e:", e) // ProgressEvent
+
+    const content = xhr.responseText;
+
+    // saveHTML(url, content);
+    console.log('file content currently is ', content);
+    document.getElementById("content").innerHTML = content;
+  }
+
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
 // step1: get file key from current url, e.g.
 // chrome-extension://jfnlfimghfiagibfigmlopnfljpfnnje/dicom.html#file:///Users/grimmer/git/itri/itri3.dcm
 const url =  window.location.href;
@@ -8,19 +27,9 @@ if (url.indexOf('file://') != -1  &&
 
   const paths = url.split("#");
   if (paths.length > 1){
-    const key = paths[1];
-    console.log("dicom html loads, after hash:", key);
-    // step2 try to get chrome storage using key from step1
-    chrome.storage.sync.get([key], function(result) {
-      const content = result[key];
-      // TODO: render the real dicom image content
-      console.log('Value currently is ', content);
-      document.getElementById("content").innerHTML = content;
-    });
+    const filePath = paths[1];
+    console.log("dicom html loads, after hash:", filePath);
 
-    // get all for debugging 
-    // chrome.storage.sync.get(null, function(result) {
-    //  console.log('whole currently is ',  result);
-    // });
+    fetchLocalFile(filePath)
   }
 }
