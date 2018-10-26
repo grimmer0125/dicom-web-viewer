74d779e7a1dd0565e55c0faedbf6d21a9cda56f8
Grimmer <grimmer0125@gmail.com>
2018-10-26 08:57:51 +0800

    Remove log

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.webRequest.onBeforeRequest.addListener(function(info) {
 
   if (info.url.indexOf('file://') === 0  &&
-      info.url.indexOf('.dcm') != -1 ) {
+      info.url.indexOf('.dcm') !== -1 ) {
 
     // console.log("request url is file://*.dcm")
 
     if (info.type !== "xmlhttprequest") {
-      console.log("not xml request, so redirect onBeforeRequest.dcm")
+      // console.log("not xml request, so redirect onBeforeRequest.dcm")
 
       return {
         redirectUrl : chrome.extension.getURL("dicom.html#"+ info.url)
