8d3e4d807df5c11d254b065c8072f81e22137bbc
Grimmer <grimmer0125@gmail.com>
2018-10-28 14:50:05 +0800

    Updates

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.webRequest.onBeforeRequest.addListener(function(info) {
+
+  if (info.url.indexOf('file://') === 0  &&
+      info.url.indexOf('.dcm') !== -1 ) {
+
+    // console.log("request url is file://*.dcm")
+
+    if (info.type !== "xmlhttprequest") {
+      // console.log("not xml request, so redirect onBeforeRequest.dcm")
+
+      return {
+        redirectUrl : chrome.extension.getURL("index.html#"+ info.url)
+      };
+    }
+  }
+}, {
+  urls : ["<all_urls>"]
+}, ["blocking"]);
