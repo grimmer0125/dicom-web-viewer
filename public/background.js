7a241bb432a7cc98bbc2150869e00f49ec5e553c
Grimmer <grimmer0125@gmail.com>
2020-06-18 01:30:01 +0800

    Click extension icon to open extension page

--- public/background.js
+++ public/background.js
@@ -1,32 +1,33 @@
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.tabs.create({ url: 'chrome-extension://fpklmaeeoagikoaiakadencfmhodampd/index.html' });
+});
+
 chrome.webRequest.onBeforeRequest.addListener(
   (info) => {
     const url = info.url.toLowerCase();
-    if (
-      url.toLowerCase().endsWith(".dcm") === false &&
-      url.endsWith(".dicom") === false
-    ) {
+    if (url.toLowerCase().endsWith('.dcm') === false && url.endsWith('.dicom') === false) {
       return;
     }
 
-    if (url.indexOf("file://") === 0) {
+    if (url.indexOf('file://') === 0) {
       // console.log("request url is file://*.dcm")
 
-      if (info.type !== "xmlhttprequest") {
+      if (info.type !== 'xmlhttprequest') {
         // console.log("not xml request, so redirect onBeforeRequest.dcm")
 
         return {
           redirectUrl: chrome.extension.getURL(`index.html#${url}`),
         };
       }
-    } else if (info.url.indexOf("http") === 0) {
+    } else if (info.url.indexOf('http') === 0) {
       return {
         redirectUrl: chrome.extension.getURL(`index.html#${url}`),
       };
     }
   },
   {
-    urls: ["<all_urls>"],
-    types: ["main_frame", "sub_frame", "image"],
+    urls: ['<all_urls>'],
+    types: ['main_frame', 'sub_frame', 'image'],
   },
-  ["blocking"]
+  ['blocking'],
 );
